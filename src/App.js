3065ed757bf1c718d7a273c1af3b91f0413b8c3d
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-22 21:01:20 +0800

    Add Person JSX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm React App!</h1>
-      <p>This is really working ^^</p>
-    </div>
-  );
-  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'am React App!</h1>
+        <p>This is really working!</p>
+        <Person />
+      </div>
+    )
+  }
 }
 
 export default App;
