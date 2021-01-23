7026506c60afb78bb61c615b8104318dde6daca3
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-23 21:13:21 +0800

    onChange handler

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons : [
-      { 'name' : 'Suara', 'age' : 20 },
-      { 'name' : 'Luna', 'age' : 24 },
-      { 'name' : 'Siy', 'age' : 21 }
+      { 'name': 'Suara', 'age': 20 },
+      { 'name': 'Luna', 'age': 24 },
+      { 'name': 'Siy', 'age': 21 }
     ],
     otherState : 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = ( newName, newAge ) => {
     // console.log("was clicked!");
     // DON'T DO THIS: this.state.persons[0].name = 'Sidiq Indrajati Yusuf';
     this.setState( {
       persons : [
-        { 'name' : 'Sidiq Indrajati Yusuf', 'age' : 20 },
-        { 'name' : 'Blanc Luna', 'age' : 24 },
-        { 'name' : 'Suara', 'age' : 22 }
+        { 'name': newName, 'age': newAge },
+        { 'name': 'Blanc Luna', 'age': 24 },
+        { 'name': 'Suara', 'age': 22 }
+      ]
+    } );
+  }
+
+  nameChangedHandler = ( event ) => {
+    this.setState( {
+      persons: [
+        { 'name': 'Suara', 'age': 20 },
+        { 'name': event.target.value, 'age': 24 },
+        { 'name': 'Siy', 'age': 21 }
       ]
     } );
   }
@@ -30,7 +40,13 @@ class App extends Component {
         <h1>Hi, I'am React App!</h1>
         <p>This is really working!</p>
         
-        <button onClick={this.switchNameHandler}>Switch name</button>
+        {/* by using () => this.eventHandler(data) be aware this can be inefficient  */}
+        {/* using this.eventHandler.bind(this, data) instead */}
+
+        <button
+          onClick={() => this.switchNameHandler('Suara!', 21)}>
+          Switch name
+        </button>
         
         <Person 
           name={this.state.persons[0].name} 
@@ -38,7 +54,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}>
+          click={this.switchNameHandler.bind(this, 'Suara!!', 22)}
+          changed={this.nameChangedHandler}>
             <p>Love Daikon (radish) love</p>
         </Person>
         <Person 
